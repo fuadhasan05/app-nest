96d668618e244fb5e08b3ccee859f9dea40483da
Fuad Hasan <fuadhasan3503@gmail.com>
2025-05-06 03:56:14 +0600

    updated

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
-import { Pagination, Autoplay } from 'swiper/modules';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import { Pagination, Autoplay } from "swiper/modules";
+import slider1 from "../assets/notion-banner.png";
+import slider2 from "../assets/coursera-banner.jpeg";
+import slider3 from "../assets/programming-hero-banner.jpeg";
 
 const Slider = () => {
-    return (
-        <div className="w-full sm:w-11/12 md:w-10/12 lg:w-8/12 mx-auto py-10 ">
-            <Swiper
-                modules={[Pagination, Autoplay]}
-                spaceBetween={30}
-                slidesPerView={1}
-                pagination={{ clickable: true }}
-                autoplay={{ delay: 3000, disableOnInteraction: false }}
-                loop={true}
-            >
-                <SwiperSlide>
-                    <div className="bg-blue-500 text-white p-10 rounded-lg">
-                        <h2 className="text-2xl font-bold">Promotion 1</h2>
-                        <p>Discover our latest app features!</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="bg-green-500 text-white p-10 rounded-lg">
-                        <h2 className="text-2xl font-bold">Promotion 2</h2>
-                        <p>Check out our newly launched app!</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="bg-purple-500 text-white p-10 rounded-lg">
-                        <h2 className="text-2xl font-bold">Promotion 3</h2>
-                        <p>Don't miss our exclusive offers!</p>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
-        </div>
-    );
+  return (
+    <div className="w-full sm:w-11/12 md:w-10/12 lg:w-11/12 mx-auto py-10 ">
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        loop={true}
+      >
+        <SwiperSlide>
+          <div className=" bg-blue-50 text-white py-20 px-40 rounded-lg">
+            <img src={slider1} alt="" className="w-320 rounded-2xl" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" bg-blue-50 text-white py-20 px-40 rounded-lg">
+          <img src={slider2} alt="" className="w-320 rounded-2xl" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="bg-blue-50 text-white py-20 px-40 rounded-lg">
+          <img src={slider3} alt="" className="w-320 rounded-2xl" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Slider;
